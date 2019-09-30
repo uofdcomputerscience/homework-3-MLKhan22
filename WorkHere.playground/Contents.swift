b51009ab5d78bb052555241ddee43af18757118f
Minh Ly <mly@udallas.edu>
2019-09-29 21:50:08 -0500

    modified WorkHere.playground

--- WorkHere.playground/Contents.swift
+++ WorkHere.playground/Contents.swift
@@ -1,4 +1,6 @@
 import Foundation
+import UIKit
+import PlaygroundSupport
 
 // This homework should be fairly simple.
 // visit https://github.com/noops-challenge/wordbot to read
@@ -10,3 +12,29 @@ import Foundation
 // decode those 50 words into an array of strings
 // print the array of words, but with each word reversed
 // print the average number of characters per word
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+struct WordList: Codable {
+    let words: [String]
+}
+
+let urlString = "https://api.noopschallenge.com/wordbot?count=50"
+if let url = URL(string: urlString) {
+    let request = URLRequest(url: url)
+    let session = URLSession(configuration: .ephemeral)
+    let task = session.dataTask(with: request) { (data, response, error) in
+        //print(String(data:data!, encoding: .utf8))
+        let wordList = try! JSONDecoder().decode(WordList.self, from: data!)
+        //print(wordList.words.count)
+        var sum = 0
+        for word in wordList.words {
+            print(String(word.reversed()))
+            sum = sum + word.count
+        }
+        let avg = sum/wordList.words.count
+        print("\nSum: " + String(sum) + ", Count: " + String(wordList.words.count))
+        print("Average number of characters per word is " + String(avg))
+    }
+    task.resume()
+}
